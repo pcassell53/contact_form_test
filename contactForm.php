18cbfc5a87acd64d909bba5c6ef3ae9d647baa5f
pcassell53 <cassellpaul53@gmail.com>
2020-02-24 15:51:12 -0500

    Another Update
    Another Update

--- contactForm.php
+++ contactForm.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset ($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $emailAddress = $_POST['emailAddress'];
